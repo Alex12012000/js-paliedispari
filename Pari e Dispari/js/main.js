c16db2e4f8ed84ad1f84934c3f0feebdb3946808
Alex12012000 <arcex.29@gmail.com>
2022-06-13 17:13:48 +0200

    Function calcolo somma

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -4,20 +4,22 @@ let userChoice = evenOddInput.toLowerCase();
 // Chiedo all'utente un numero da 1 a 5
 const evenOddUserNmber = parseInt(prompt('Scegli un numero da 1 a 5'));
 // Variabile che racchiude ai il numero random generato dalla funzione (numero che userà il computer per giocare contro l'utente)
+// e il calcolo del numero utente con il numero random del computer
 const aiResult = aiEvenOdd(1, 5);
-// Risultato fra il numero dell'utente e quello del computer
-const finalResult = evenOddUserNmber + aiResult;
+const sum = finalresult(evenOddUserNmber, aiResult)
 
+
+// PRINT
 console.log(evenOddUserNmber);
 console.log(aiResult);
-console.log(finalResult);
+console.log(sum);
 
 // Se l'utente ha scelto pari e il risultato finale è divisibile per 2, l'utente vince
 // Se l'utente ha scelto dispari e il risultato finale non è divisibile per 2, l'utente vince
 // Altrimenti l'utente perde
-if(userChoice === 'pari' && finalResult % 2 === 0) {
+if(userChoice === 'pari' && sum % 2 === 0) {
     alert('Pari! Hai Vinto');
-} else if (userChoice === 'dispari' && finalResult % 2 !== 0) {
+} else if (userChoice === 'dispari' && sum % 2 !== 0) {
     alert('Dispari! Hai Vinto!');
 } else {
     alert('Hai perso!');
@@ -26,4 +28,9 @@ if(userChoice === 'pari' && finalResult % 2 === 0) {
 // Funzione che calcola un numero casuale per il computer
 function aiEvenOdd(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
+
+// Funzione che calcola la somma fra il il numero dell'utente e il nomero del computer
+function finalresult(parm1, param2) {
+    return parm1 + param2
+}
